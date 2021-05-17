fc0d558eb83a00eda3898528b133d207f886653f
drA666 <43187328+drA666@users.noreply.github.com>
2021-05-17 21:58:11 +0300

    Update server.go

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -8,8 +8,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/roman-mazur/design-practice-2-template/httptools"
-	"github.com/roman-mazur/design-practice-2-template/signal"
+	"github.com/SofiaMazur/razur_s2_lab2/httptools" 
+	"github.com/SofiaMazur/razur_s2_lab2/signal"
 )
 
 var port = flag.Int("port", 8080, "server port")
